d700193e56c37f62a54fd9f7e9b843540f952ded
MUKILTEO\1549673 <1549673@mukilteo.wednet.edu>
2025-12-04 13:20:24 -0800

    skill tree button stat code

--- Assets/Skill_Tree.cs
+++ Assets/Skill_Tree.cs
@@ -6,19 +6,20 @@ public class Skill_Tree : MonoBehaviour
     public Player_Movement player_Movement;
     public Enemy_AI enemy_AI;
     public Stats stats;
+    public SpawnHitBox spawnHitBox;
 
     public void Movement()
     {
-        player_Movement.movespeed = player_Movement.movespeed + 1;
+        player_Movement.movespeed += 1;
     }
     public void Enemy_Speed_and_Move_Speed()
     {
-        player_Movement.movespeed = player_Movement.movespeed + 2;
-        enemy_AI.movespeed = enemy_AI.movespeed + 2;
+        stats.Speed += 2;
+        enemy_AI.movespeed += 2;
     }
     public void Move_Speed()
     {
-        player_Movement.movespeed = player_Movement.movespeed + 1;
+        stats.Speed += 1;
     }
     public void Farming()
     {
@@ -36,21 +37,34 @@ public class Skill_Tree : MonoBehaviour
     {
 
     }
-    public void Attack()
+    public void Grow_SpeedE()
     {
 
+    }
+    public void Grow_SpeedS()
+    {
+
+    }
+    public void Attack()
+    {
+        stats.Attack_Power += 10;
+        stats.Attack_Speed += 1;
     }
     public void Attack_Power()
     {
-        stats.Attack_Power =+ 10;
+        stats.Attack_Power += 10;
     }
     public void Attack_Speed()
-    {stats.Attack_Speed =+ 1;
-
+    {
+        stats.Attack_Speed += + 1;
     }
     public void Knockback()
     {
-
+        spawnHitBox.knockbackForce += 50;
+    }
+    public void Stun()
+    {
+        spawnHitBox.stunDuration += 1;
     }
     public void Return()
     {
