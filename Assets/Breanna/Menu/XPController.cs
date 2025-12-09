dab8a7966ad84fdd7346e3fc26c521d73a2959a3
MUKILTEO\1717489 <Breannashanonray@gmail.com>
2025-12-09 13:37:43 -0800

    LEVELING SYSTEM AHHH :DDD

---
+++ Assets/Breanna/Menu/XPController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+public class XPController : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI LevelText;
+    [SerializeField] private TextMeshProUGUI ExperienceText;
+    [SerializeField] private int Level;
+    public float CurrentXp;
+    [SerializeField] private float TargetXp;
+    [SerializeField] private Image XpProgressBar;
+
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CurrentXp += 25;
+        }
+
+
+        ExperienceController();
+    }
+
+    public void ExperienceController()
+    {
+        LevelText.text = "Level : " + Level.ToString();
+        XpProgressBar.fillAmount = (CurrentXp / TargetXp);
+
+        if(CurrentXp >= TargetXp)
+        {
+            CurrentXp = CurrentXp - TargetXp;
+            Level++;
+            TargetXp *= (float)1.25;
+        }
+    }
+
+    public void EnemyXp1()
+    {
+        CurrentXp += 10;
+    }
+    public void EnemyXp2()
+    {
+        CurrentXp += 25;
+    }
+    public void EnemyXp3()
+    {
+        CurrentXp += 50;
+    }
+}
